113c7212f1e455c2ec84ed8e5ef1bdab2e27b56e
malimikillua <moshood988@gmail.com>
2024-11-27 15:38:20 +0100

    scafflod improvments

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import Link from "next/link";
+import Image from "next/image";
+
+const mockUrls = [
+  "https://utfs.io/f/pglkKhbB4exf63dz6tU9OID8npAgiq1BYh4VQ5CmRKNZSlvF",
+  "https://utfs.io/f/pglkKhbB4exfkZLu3SJZsquUEJwdKbHkiypIcaolVSYNtmjW",
+  "https://utfs.io/f/pglkKhbB4exf1sQMI5ykJAqDQo3EVa0I5HurS7myX92kbx6F",
+  "https://utfs.io/f/pglkKhbB4exfS0AC7nPwVihREHfmOMqsZCnax2e5PK04yb9T",
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello world
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+          <div key={index + 1} className="w-48">
+            <Link href={image.url}>
+              <img src={image.url} alt={image.url} />
+            </Link>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
