d0c4f7bb2898942a4c9c01df3b3e662a06bc6f08
cyberkillua <moshood988@gmail.com>
2024-11-29 12:23:19 +0100

    auth done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
 import Link from "next/link";
 import Image from "next/image";
 import { db } from "../server/db";
+import { SignedIn, SignedOut } from "@clerk/nextjs";
 
 export const dynamic = "force-dynamic";
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy: (model, { desc }) => desc(model.id),
   });
+  return (
+    <div className="flex flex-wrap gap-4">
+      {[...images, ...images, ...images].map((image, index) => (
+        <div key={index + 1} className="flex w-48 flex-col">
+          <img src={image.url} alt={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
 
+export default async function HomePage() {
   return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {[...images, ...images, ...images].map((image, index) => (
-          <div key={index + 1} className="flex w-48 flex-col">
-            <img src={image.url} alt={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl">Please make you to sign in</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
